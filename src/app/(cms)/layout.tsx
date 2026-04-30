cd4a5c95213921ab19d7b86761f3ad5a9bc5747b
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2026-04-30 16:45:53 +0530

    feat: add Vercel Analytics to all layouts

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,7 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <body id="outstatic">{children}</body>
+      <body id="outstatic">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
